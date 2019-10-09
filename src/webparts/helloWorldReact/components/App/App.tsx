f83ba348b991ab4fe7ef054f8b017c8631143a19
Boris Korobeinikov <slon2k@gmail.com>
2019-10-09 17:15:25 +0600

    state created

--- src/webparts/helloWorldReact/components/App/App.tsx
+++ src/webparts/helloWorldReact/components/App/App.tsx
@@ -4,21 +4,21 @@ import UserInfo from "../UserInfo";
 import TodoList from "../TodoList";
 import styles from "./App.module.scss";
 import apiService from "../../services/api.service";
+import {useState} from "react";
 
 export const App: React.FunctionComponent<IAppProps> = ({email, name, description, absoluteUrl}) => {
+  const [todos, setTodos] = useState([]);
   const api = new apiService(absoluteUrl);
-  api.getListItems().then(res => console.log("App", res));
-  const todoItems = [];
-  todoItems.push({id: "1", title: "first", completed: false});
-  todoItems.push({id: "2", title: "second", completed: true});
-
+  const res = api.getListItems().then(result => setTodos(result));
+  //todoItems.push({id: "1", title: "first", completed: false});
+  //todoItems.push({id: "2", title: "second", completed: true});
   return (
     <div className={styles.App}>
       <h2>App component</h2>
       <h3> {description} </h3>
       <UserInfo email={email} name={name}/>
-      {todoItems.length > 0
-        ? <TodoList items={todoItems}/>
+      {todos.length > 0
+        ? <TodoList items={todos}/>
         : <div>No items to show</div>
       }
     </div>
--- src/webparts/helloWorldReact/services/api.service.ts
+++ src/webparts/helloWorldReact/services/api.service.ts
@@ -1,6 +1,12 @@
 const URL = '/sites/dev/_api/Web/Lists(guid\'44da09e2-9766-40c7-a48a-4680f6ef4700\')/Items';
 const headers = { accept: "application/json;odata=verbose" };
 
+const convertItem = (item) => {
+  console.log(item);
+  const {Id, Title, Completed} = item;
+  return {id: Id, title: Title, completed: Completed};
+}
+
 export default class apiService {
   private baseUrl: string;
 
@@ -8,10 +14,18 @@ export default class apiService {
     this.baseUrl = baseUrl;
   }
 
+
   public getListItems = async () => {
-    return await fetch(`${this.baseUrl}${URL}`, {headers})
-      .then(result => result.json())
+    const result = await fetch(`${this.baseUrl}${URL}`, {headers});
+
+    if (!result.ok) {
+      throw new Error(`Could not fetch data, received ${result.status}`);
+    }
+
+    return await result.json()
       .then(({d}) => d.results)
-      .catch(error => console.error(error));
+      .then(res => res.map(item => convertItem(item)));
   }
+
+
 }
